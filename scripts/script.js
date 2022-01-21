a0ee6c6e6b793fffb91616adcc8f47bcd508be71
k4kaylee <roman.kulpeksha@mail.ru>
2022-01-21 12:47:01 +0300

    Testing dark and light theme switch

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,42 @@ function showPage() {
 function showList(){
   document.getElementById("list").style.display = "block";
 }
+
+
+
+//Реализация переключения цветовой темы
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("change", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "styles/light.css";
+    let darkTheme = "styles/dark.css";
+
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+
+    if(currTheme == lightTheme)
+    {
+   	 currTheme = darkTheme;
+   	 theme = "dark";
+    }
+    else
+    {
+   	 currTheme = lightTheme;
+   	 theme = "light";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    Save(theme);
+}
+
+function Save(theme)
+{
+    var Request = new XMLHttpRequest();
+    Request.open("GET", "./themes.php?theme=" + theme, true); //У вас путь может отличаться
+    Request.send();
+}
